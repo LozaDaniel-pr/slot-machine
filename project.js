664fda20c18e1981c467a521ea2bbecacd966324
Loza <97474199+Lolah101@users.noreply.github.com>
2023-10-01 07:44:03 +0100

    Initial commit

---
+++ project.js
@@ -0,0 +1,25 @@
+// 1. Deposet some money
+// 3. Determine number of lines to bet on
+// 2. Collect a bet amount
+// 4. Spin the slot machine
+// 5. check if the user won
+// 6. give the user their winnings
+// 7. play again
+
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+  while (true) {
+    const depositAmount = prompt("Enter a deposit amount: ");
+    const numberDepositAmount = parseFloat(depositAmount);
+
+    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+      console.log("Invalid deposit amount, try again. ");
+    } else {
+        return numberDepositAmount;
+    }
+  }
+};
+
+const depositAmount = deposit();
+console.log(depositAmount);
